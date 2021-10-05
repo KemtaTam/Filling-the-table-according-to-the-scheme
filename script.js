0f886007e70f4cf4d5625cf84906daa345cde31d
Максим <41415269+KemtaTam@users.noreply.github.com>
2021-10-05 18:38:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,85 @@ document.querySelectorAll('input').forEach(input => {
 let tbody = document.getElementsByTagName("tbody")[0];
 let header = document.getElementsByTagName("header")[0];
 let body = document.getElementsByTagName("body")[0];
+let canvas = document.getElementsByTagName('canvas')[0];
+let ctx = canvas.getContext('2d');
 
+//генератор задания
 let num = getRandomIntInclusive(2, 3);
-let image = document.createElement("img");
-image.setAttribute("src", "images/" + num + "schema.svg");
+canvas.style.backgroundImage = 'url(images/' + num + 'schema.svg)';
 if(num==3){
-	image.style.width = "700px";
+	canvas.setAttribute('width', '700px');
+	canvas.setAttribute('height', '330px');
 }
-header.after(image);
 
-tables = {
+let draw = document.getElementsByClassName('draw-img')[0];
+let clear = document.getElementsByClassName('clear-img')[0];
+let clearAll = document.getElementsByClassName('clearAll-btn')[0];
+//функция рисования
+function drawing(){
+	canvas.onmousedown = (event) => {
+		let x = event.offsetX;
+		let y = event.offsetY;
+
+		ctx.beginPath();
+		ctx.lineJoin = 'round';
+		ctx.lineCap = 'round';
+		ctx.moveTo (x,y);
+		ctx.lineTo (x,y);
+		ctx.lineWidth = '1';
+		ctx.strokeStyle = 'red';
+		ctx.stroke();
+
+		canvas.onmousemove = (event) => {
+			let x = event.offsetX;
+			let y = event.offsetY;
+			ctx.lineTo (x,y);
+			ctx.stroke();
+		}
+		canvas.onmouseup = () =>{
+			canvas.onmousemove = null;
+		}
+		canvas.onmouseout = function(){
+			canvas.onmousemove = null;
+		}
+	}
+}
+//функция стирания
+function clearing(){
+	canvas.onmousedown = (event) => {
+		let x = event.offsetX;
+		let y = event.offsetY;
+
+		ctx.beginPath();
+		ctx.clearRect(x-3,y-3,6,6)
+		ctx.stroke();
+
+		canvas.onmousemove = (event) => {
+			let x = event.offsetX;
+			let y = event.offsetY;
+			ctx.clearRect(x-3,y-3,6,6)
+		}
+		canvas.onmouseup = () =>{
+			canvas.onmousemove = null;
+		}
+		canvas.onmouseout = function(){
+			canvas.onmousemove = null;
+		}
+	}
+}
+draw.onclick = function(){
+	canvas.style.cursor = 'crosshair';
+	drawing();
+}
+clear.onclick = function(){
+	canvas.style.cursor = 'not-allowed';
+	clearing();
+}
+clearAll.onclick = function(){
+	ctx.clearRect(0,0, 1000,1000);
+}
+
+tables = {	
 	1: {
 		inputVar: ['x0 ', 'x1'],
 		inputChar: ['00', '01', '10', '11'],
@@ -37,7 +106,16 @@ tables = {
 		states: ['00', '01', '10', '11',]
 	}
 }; 
-answer = {
+answer = { 
+	1: {
+		ans: ['111/1', '', '111/1', '101/0', '', '011/1', '', '011/1',
+			  '111/1', '', '111/1', '101/0', '', '011/1', '', '011/1',
+			  '010/1', '', '010/1', '010/1', '', '010/1', '', '010/1',
+			  '011/1', '', '011/1', '011/1', '', '011/1', '', '011/1'],
+		delState: ['2', '5', '7'],
+		nondelState: ['1', '3', '4', '6', '8'],
+	},
+
 	2: {
 		ans: ['10/0', '', '10/0', '10/0',
 			  '11/0', '', '11/0', '10/0',
@@ -56,114 +134,105 @@ answer = {
 		delState: ['4'],
 		nondelState: ['1', '2', '3']
 	}
-
 }; 
 
 function buildTable()
 {
-		//первая строка
-		let tr1 = document.createElement("tr");
-		tr1.setAttribute("class", "first_row");
-		tbody.appendChild(tr1);
-
-		//первая ячейка первой строки
-		let th1 = document.createElement("th");
-		th1.setAttribute("class", "first_col t_1-1");
-		for(let i=0; i<tables[num].inputVar.length; i++){
-			//переменная
-			let x = document.createElement('class');
-			x.textContent = tables[num].inputVar[i][0];
-			x.style.backgroundColor = "inherit";
-			x.style.fontSize = '18px';
-			th1.appendChild(x);
-			//индекс
-			let ind = document.createElement('class');
-			ind.textContent = tables[num].inputVar[i][1];
-			ind.style.backgroundColor = "inherit"
-			ind.style.fontSize = '11px';
-			th1.appendChild(ind);
-		}
-		th1.setAttribute("rowspan", 2);
-		tr1.appendChild(th1);
-
-		//вторая ячейка первой строки
-		let th2 = document.createElement("th");
-		for(let i=0; i<tables[num].triggers.length; i++){
-			let dd = document.createElement("class");
-			dd.style.marginLeft = "3%";
-			dd.style.marginRight = "3%";
-			dd.style.backgroundColor = "inherit";
-			dd.textContent = tables[num].triggers[i];
-			th2.appendChild(dd);
-		}	
-		th2.setAttribute("colspan", tables[num].states.length);
-		tr1.appendChild(th2);
-
-		//состояния
+	//первая строка
+	let tr1 = document.createElement("tr");
+	tr1.setAttribute("class", "first_row");
+	tbody.appendChild(tr1);
+	//первая ячейка первой строки
+	let th1 = document.createElement("th");
+	th1.setAttribute("class", "first_col t_1-1");
+	for(let i=0; i<tables[num].inputVar.length; i++){
+		//переменная
+		let x = document.createElement('class');
+		x.textContent = tables[num].inputVar[i][0];
+		x.style.backgroundColor = "inherit";
+		x.style.fontSize = '18px';
+		th1.appendChild(x);
+		//индекс
+		let ind = document.createElement('class');
+		ind.textContent = tables[num].inputVar[i][1];
+		ind.style.backgroundColor = "inherit"
+		ind.style.fontSize = '11px';
+		th1.appendChild(ind);
+	}
+	th1.setAttribute("rowspan", 2);
+	tr1.appendChild(th1);
+	//вторая ячейка первой строки
+	let th2 = document.createElement("th");
+	for(let i=0; i<tables[num].triggers.length; i++){
+		let dd = document.createElement("class");
+		dd.style.marginLeft = "3%";
+		dd.style.marginRight = "3%";
+		dd.style.backgroundColor = "inherit";
+		dd.textContent = tables[num].triggers[i];
+		th2.appendChild(dd);
+	}	
+	th2.setAttribute("colspan", tables[num].states.length);
+	tr1.appendChild(th2);
+	//состояния
+	let tr = document.createElement("tr");
+	tr.setAttribute("class", "states");
+	tbody.appendChild(tr);
+	//ячейки состояний
+	for(let i=0; i<tables[num].states.length; i++){
+		let th = document.createElement("th");
+		th.textContent = tables[num].states[i];
+		tr.appendChild(th);
+	}
+	
+	//строки входных символов и инпутов
+	for(let i=0; i<tables[num].inputChar.length; i++)
+	{
 		let tr = document.createElement("tr");
-		tr.setAttribute("class", "states");
+		tr.setAttribute("class", "second_row");
 		tbody.appendChild(tr);
-		//ячейки состояний
-		for(let i=0; i<tables[num].states.length; i++){
+		//создаю ячейки входных символов
+		let th1 = document.createElement("th");
+		th1.setAttribute("class", "first_col input_char");
+		th1.textContent = tables[num].inputChar[i];
+		tr.appendChild(th1);
+		//создаю остальные ячейки
+		for(let j=0; j<tables[num].states.length; j++)
+		{
 			let th = document.createElement("th");
-			th.textContent = tables[num].states[i];
+			let input_value = document.createElement("input");
+			input_value.setAttribute('class', 'valueOfTable');
+			input_value.type = "text";
+			if(num == 1){
+				input_value.placeholder = '000/0';;
+			} else if(num == 2 || num == 3){
+				input_value.placeholder = '00/0';;
+			}
+			input_value.name = "input_value";
+			input_value.autocomplete="off";
+			th.appendChild(input_value);
 			tr.appendChild(th);
 		}
-		
-		//строки входных символов и инпутов
-		for(let i=0; i<tables[num].inputChar.length; i++)
-		{
-			let tr = document.createElement("tr");
-			tr.setAttribute("class", "second_row");
-			tbody.appendChild(tr);
-			//создаю ячейки входных символов
-			let th1 = document.createElement("th");
-			th1.setAttribute("class", "first_col input_char");
-			th1.textContent = tables[num].inputChar[i];
-			tr.appendChild(th1);
-
-			//создаю остальные ячейки
-			for(let j=0; j<tables[num].states.length; j++)
-			{
-				let th = document.createElement("th");
-				let input_value = document.createElement("input");
-
-				input_value.setAttribute('class', 'valueOfTable');
-				input_value.type = "text";
-				if(num == 1){
-					input_value.placeholder = '000/0';;
-				} else if(num == 2 || num == 3){
-					input_value.placeholder = '00/0';;
-				}
-				input_value.name = "input_value";
-				input_value.autocomplete="off";
-
-				th.appendChild(input_value);
-				tr.appendChild(th);
-			}
-		}
-
-		//кнопки удаления
-		let trdel = document.createElement("tr");
-		trdel.setAttribute("id", "delRow");
-		tbody.appendChild(trdel);
-		//невидимая кнопка
-		let nonth = document.createElement("th");
-		nonth.setAttribute("id", "b1");
-		trdel.appendChild(nonth);
-		//видимые кнопки
-		for(let i=0; i<tables[num].states.length; i++)
-		{
-			let th = document.createElement("th");
-			th.setAttribute("class", "delRow");
-			let butDel = document.createElement("button");
-			butDel.setAttribute("class", "bDelCol");
-			butDel.setAttribute("type", "button");
-			butDel.textContent = 'X';
-
-			th.appendChild(butDel);
-			trdel.appendChild(th);
-		}
+	}
+	//кнопки удаления
+	let trdel = document.createElement("tr");
+	trdel.setAttribute("id", "delRow");
+	tbody.appendChild(trdel);
+	//невидимая кнопка
+	let nonth = document.createElement("th");
+	nonth.setAttribute("id", "b1");
+	trdel.appendChild(nonth);
+	//видимые кнопки
+	for(let i=0; i<tables[num].states.length; i++)
+	{
+		let th = document.createElement("th");
+		th.setAttribute("class", "delRow");
+		let butDel = document.createElement("button");
+		butDel.setAttribute("class", "bDelCol");
+		butDel.setAttribute("type", "button");
+		butDel.textContent = 'X';
+		th.appendChild(butDel);
+		trdel.appendChild(th);
+	}
 }
 buildTable();
 
@@ -218,8 +287,10 @@ function delCol()
 			for(let j=0; j<tbody.children.length-2; j++){
 				tbody.children[j+2].children[i+1].firstElementChild.value = "";
 				tbody.children[j+2].children[i+1].style.visibility = "hidden";
+				tbody.children[j+2].children[i+1].style.border = "none";
 			}
 			tbody.children[1].children[i].style.visibility = "hidden";
+			tbody.children[1].children[i].style.border = "none";
 		})
 	}
 }
